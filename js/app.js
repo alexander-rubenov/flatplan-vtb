2d7736a229e6582441f33a1d16fb49bf288e935c
Alexander Rubenov <alexander.rubenov@gmail.com>
2019-08-12 17:51:25 +0300

    Added section how-it-works with addaptive

--- js/app.js
+++ js/app.js
@@ -1,2 +1,12 @@
 'use strict';
 
+let clientHeight = document.documentElement.clientHeight;
+let clientWidth = document.documentElement.clientWidth;
+
+console.log(clientHeight);
+
+let pageHeader = document.querySelector('.page-header');
+
+if (clientWidth <= 536) {
+    pageHeader.style.height = clientHeight;
+}
