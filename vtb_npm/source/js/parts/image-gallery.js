baf5564dd34deea64c171853453a0a7acd9b97ba
Alexander Rubenov <alexander.rubenov@gmail.com>
2019-08-15 19:26:40 +0300

    Added webp

--- vtb_npm/source/js/parts/image-gallery.js
+++ vtb_npm/source/js/parts/image-gallery.js
@@ -6,15 +6,18 @@
 
     function changeMainScreenImage(event) {
         let target= event.target;
-
         if (!target.classList.contains('consultation-with-designer__image')) return;
 
         let
             srcMainScreenImage = mainScreenImage.src.slice(target.src.indexOf('vtb_static/img/consultation-with-designer-')),
-            srcImg = target.src.slice(target.src.indexOf('vtb_static/img/consultation-with-designer-'));
-        
+            srcWebpMainScreenImage = mainScreenImage.previousElementSibling.srcset,
+            srcImg = target.src.slice(target.src.indexOf('vtb_static/img/consultation-with-designer-')),
+            srcWebpImg = target.previousElementSibling.srcset;
+
         mainScreenImage.src = srcImg;
+        mainScreenImage.previousElementSibling.srcset = srcWebpImg;
         target.src = srcMainScreenImage;
+        target.previousElementSibling.srcset = srcWebpMainScreenImage;
     }
 
     imageGallery.addEventListener('click', changeMainScreenImage);
