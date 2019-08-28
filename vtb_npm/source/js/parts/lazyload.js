3db41a694b71300bf993e1b067c6c0902432b67d
alexander-rubenov <alexander.rubenov@gmail.com>
2019-08-28 15:19:24 +0300

    Changed lazyload

--- vtb_npm/source/js/parts/lazyload.js
+++ vtb_npm/source/js/parts/lazyload.js
@@ -1,56 +1,5 @@
 (() => {
 
-  // const lazyPictures = [].slice.call(document.querySelectorAll("picture.lazy"));
-
-  // function lazyloadOnIntersection() {
-  //   if ("IntersectionObserver" in window) {
-  //     let lazyPictureObserver = new IntersectionObserver(function(entries, observer) {
-  //       entries.forEach(function(entry) {
-  //         if (entry.isIntersecting) {
-  //           let lazyPicture = entry.target;
-  //           let lazyWebpImage = lazyPicture.querySelector('[type="image/webp"]');
-  //           let lazyImage = lazyPicture.querySelector('img');
-  //
-  //           lazyWebpImage.srcset = lazyWebpImage.dataset.src;
-  //           lazyImage.src = lazyImage.dataset.src;
-  //
-  //           lazyPicture.setAttribute('data-picture', 'opacity-1');
-  //           lazyPicture.classList.remove("lazy");
-  //           lazyPictureObserver.unobserve(lazyPicture);
-  //         }
-  //       });
-  //     });
-  //
-  //     lazyPictures.forEach(function(lazyPicture) {
-  //       lazyPictureObserver.observe(lazyPicture);
-  //     });
-  //   }
-  // }
-  //
-  // document.addEventListener("DOMContentLoaded", lazyloadOnIntersection);
-
-
-  // function lazyloadOnLoadPage() {
-  //   let lazyPictureObserver = new IntersectionObserver((entries, observer) => {
-  //     entries.forEach(function(entry) {
-  //       let lazyPicture = entry.target;
-  //       let lazyWebpImage = lazyPicture.querySelector('[type="image/webp"]');
-  //       let lazyImage = lazyPicture.querySelector('img');
-  //
-  //       lazyWebpImage.srcset = lazyWebpImage.dataset.src;
-  //       lazyImage.src = lazyImage.dataset.src;
-  //
-  //       lazyPicture.setAttribute('data-picture', 'opacity-1');
-  //       lazyPicture.classList.remove("lazy");
-  //       lazyPictureObserver.unobserve(lazyPicture);
-  //     });
-  //   });
-  //
-  //   lazyPictures.forEach(function(lazyPicture) {
-  //     lazyPictureObserver.observe(lazyPicture);
-  //   });
-  // }
-
   function lazyloadOnLoadPage() {
     const lazyPictures = [].slice.call(document.querySelectorAll("picture.lazy"));
 
@@ -66,7 +15,6 @@
       lazyPicture.classList.remove("lazy");
     });
   }
-
 
   document.addEventListener('DOMContentLoaded', lazyloadOnLoadPage);
 
